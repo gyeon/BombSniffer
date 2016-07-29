2d915d50294d6e12d8e5a13b7e5967422a35f759
Ryan Palermo <stix250@gmail.com>
2016-07-29 18:10:58 -0400

    add movie creation to controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,7 +1,8 @@
 var moviesController = (function(){
   function searchMovies() {
     var userInput = $('#searchTerms').val()
-    // constructor(name, year, rating, review, poster, studio, boxOffice) {
+
+    $('#movies').empty()
     $.ajax({
       method: "GET",
       url: `https://api.themoviedb.org/3/search/person?query=${userInput}&api_key=bcd69b485671c77289868b4acf21bcf0`
@@ -11,7 +12,15 @@ var moviesController = (function(){
         method: "GET",
         url: `https://api.themoviedb.org/3/discover/movie?with_cast=${personId}&sort_by=vote_average.asc&api_key=bcd69b485671c77289868b4acf21bcf0`
       }).done(function(response) {
-        $('#movies').append(`<p> ${response.results[0].title} </p>`)
+        debugger
+
+        var $title = response.results[0].title
+        var $year = response.results[0].release_date.split("-")[0]
+        var $overview = response.results[0].overview
+        var $poster = "http://image.tmdb.org/t/p/w500" + response.results[0].poster_path
+        $('#movies').append(`<p> ${$title} </p>`)
+// constructor(name, year, rating, review, poster, studio, boxOffice) {
+        new Movie($title, $year, $overview, $poster)
       })
     })
   }
